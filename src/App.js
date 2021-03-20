c80af673605d8a2fc0ae2817c5012e064a2cfa7a
Olivier GUZZI <oguzzi@shopstyle.com>
2021-03-20 04:23:57 +0100

    Add streamed BE video

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const videoUrl = "https://elite-agents-backend.herokuapp.com/video/stream/url/mp4?url=" +
+        "https://user-images.githubusercontent.com/34119356/111480600-75d5ea00-8732-11eb-8016-e40db5f8ab3f.mp4";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video autoPlay="true" src={ videoUrl } type="video/mp4">
+
+        </video>
       </header>
     </div>
   );
